2e42a1da6b4cad750b28f8d5cc54b368f1f3c405
oddlyspaced <oddlyspaced1@gmail.com>
2020-07-08 08:11:59 +0200

    laurel_sprout: Add evox overlay definition to device.mk

--- device.mk
+++ device.mk
@@ -95,6 +95,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-evox
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
